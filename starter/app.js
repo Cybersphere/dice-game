eaaad0b4731e1c17946e8ef45ef043e9545035fc
Cybersphere <e.izukaev@elama.ru>
2018-08-06 18:39:26 +0300

    random dice + reset score

--- starter/app.js
+++ starter/app.js
@@ -3,13 +3,18 @@ let roundScore = 0;
 
 let activePlayer = 0;
 
-let dice = Math.floor(Math.random()*6) + 1;
-console.log(dice);
-
-// document.querySelector("#current-" + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-let x = document.querySelector('#score-0').textContent;
-console.log(x);
-
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+function btn() {
+    let dice = Math.floor(Math.random()*6) + 1;
+    let diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+}
+
+document.querySelector('.btn-roll').addEventListener('click', btn);
