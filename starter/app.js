77e1aaa5e877d0be881e5e69742bc13af5f719cb
Cybersphere <e.izukaev@elama.ru>
2018-08-06 19:01:53 +0300

    Добавляет активное состояние игрока

--- starter/app.js
+++ starter/app.js
@@ -15,6 +15,26 @@ function btn() {
     let diceDOM = document.querySelector('.dice')
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
+
+    if (dice !== 1) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        if (activePlayer === 0) {
+            activePlayer = 1;
+        } else {
+            activePlayer = 0;
+        }
+
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.dice').style.display = 'none';
+    }
 }
 
 document.querySelector('.btn-roll').addEventListener('click', btn);
